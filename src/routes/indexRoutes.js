ca9c3e5dd56cc7079a5c0091236091e66c7125f5
lio85 <lionelprats2021@gmail.com>
2021-10-13 23:04:29 -0300

    avanzando

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const validationLogin= require('./../middlewares/validationLogin');
 const refusedLogin= require('./../middlewares/refusedLogin');
+const adminLogged= require('../middlewares/adminLogged');
 
 
 const indexController= require('./../controllers/indexController');
 
 router.get('/', refusedLogin, indexController.login);
 router.post('/', validationLogin, indexController.processLogin);
-router.get('/logout', indexController.logout);
+router.get('/logout', adminLogged, indexController.logout);
 
 module.exports= router;
